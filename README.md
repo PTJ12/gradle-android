31fe142b79065d0eedccdc23ca99a063e045cdbe
ut <15391493308@163.com>
2022-08-04 01:00:24 +0800

    修改fragment布局内容

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # gradle-android
+后端接口地址 [gradle](http://81.68.234.21:8080/doc.html)
 与gradle后台管理适配的android前台 实现登录退出功能
--- app/src/main/java/cn/ut/application/network/NetworkConfig.java
+++ app/src/main/java/cn/ut/application/network/NetworkConfig.java
@@ -1,5 +1,5 @@
 package cn.ut.application.network;
 
 public class NetworkConfig {
-    protected static final String BASE_URL = "http://192.168.0.105:9090/";
+    protected static final String BASE_URL = "http://81.68.234.21:8080/";
 }
